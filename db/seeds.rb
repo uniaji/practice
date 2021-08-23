92cb622d38bc26971b823414a399bc23e4809dc2
uniaji <info@unin.co.jp>
2021-08-23 13:35:30 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1,name: 'Aji', title: 'あじ', content: 'アメショ')
+Question.create(id: 2,name: 'Uni', title: 'うに', content: 'マンチカン')
+Question.create(id: 3,name: 'Fugu', title: 'ふぐ', content: '？')
